bb567548ba1a995b7c526b4b1764c483e66a8806
Michael Scott <mas4u19@gmail.com>
2022-02-20 15:07:30 -0500

    feature-twitter-feed

--- Project1/force-app/main/default/lwc/twitterHandlerLwc/twitterHandlerLwc.js
+++ Project1/force-app/main/default/lwc/twitterHandlerLwc/twitterHandlerLwc.js
@@ -9,21 +9,21 @@ export default class TwitterHandlerLwc extends LightningElement {
     @api recordId
 
     //return full twitter url with default value or reactive value based on wire service
-    twitterHandler = 'salesforceorg';
+    twitterHandler = 'salesforceorg'
     get fullUrl() {
-        return 'https://velocity-customization-5212-dev-ed--c.visualforce.com/apex/TwitterFeedPage?twitterHandle=$this.twitterHandler';
+        console.log(this.twitterHandler);
+        return `https://velocity-customization-5212-dev-ed--c.visualforce.com/apex/TwitterFeedPage?twitterHandle=${this.twitterHandler}`
     }
 
-    //use Wire adapter w/ lightning data service to get users twitter handle field from Contact object
+    //use Wire adapter w/ lightning data service to get users twitter handle
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     wiredRecord({ error, data }) {
 
         if (data) {
-            console.log(data.fields.Twitter_Handle__c.value);
-            this.twitterHandler = data.fields.Twitter_Handle__c.value;
+            this.twitterHandler = data.fields.Twitter_Handle__c.value
         }
         if (error) {
-            console.error(error);
+            console.error(error)
         }
     }
 }
