85044cb8928b52fe12ba4d887d4bc02a0eee917f
Michael Scott <mas4u19@gmail.com>
2022-02-28 15:20:27 -0500

    social-media

--- Project1/force-app/main/default/lwc/twitterHandlerLwc/twitterHandlerLwc.js
+++ Project1/force-app/main/default/lwc/twitterHandlerLwc/twitterHandlerLwc.js
@@ -31,20 +31,4 @@ export default class TwitterHandlerLwc extends LightningElement {
             console.error(error)
         }
     }
-
-    firstName = '';
-    lastName = '';
-
-    handleChange(event) {
-        const field = event.target.name;
-        if (field === 'firstName') {
-            this.firstName = event.target.value;
-        } else if (field === 'lastName') {
-            this.lastName = event.target.value;
-        }
-    }
-
-    get uppercasedFullName() {
-        return `${this.firstName} ${this.lastName}`.toUpperCase();
-    }
 }
